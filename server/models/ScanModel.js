54fb03fc39df611ddd15b42071e43e993f622df9
Thalleen <cnthalleen@gmail.com>
2024-11-23 03:57:49 +0530

    Done

--- server/models/ScanModel.js
+++ server/models/ScanModel.js
@@ -4,7 +4,7 @@ const scanSchema = new mongoose.Schema({
   fileName: { type: String, required: true },
   base64Image:{type:String, required:true},
   detectedFields: { type: Object, required: true },
-  classification: { type: String, required: true }, // This is causing the error
+  classification: { type: String, required: true }, 
   createdAt: { type: Date, default: Date.now },
 });
 
