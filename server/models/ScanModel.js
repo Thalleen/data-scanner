e89bc2f1039d97ab664250102ff4febbb73c4717
Thalleen <144317300+Thalleen@users.noreply.github.com>
2024-12-16 23:23:50 +0530

    Update ScanModel.js

--- server/models/ScanModel.js
+++ server/models/ScanModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const scanSchema = new mongoose.Schema({
   fileName: { type: String, required: true }, 
-  base64Image:{type:String, required:true},
+  base64Image:{type:String, required:true}, 
   detectedFields: { type: Object, required: true },
   classification: { type: String, required: true },  
   createdAt: { type: Date, default: Date.now },
