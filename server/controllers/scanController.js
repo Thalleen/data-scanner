99ae7f8997ef2a8d284d8c4a1a6fe547078e5cf3
Thalleen <144317300+Thalleen@users.noreply.github.com>
2024-12-19 22:28:00 +0530

    Update scanController.js

--- server/controllers/scanController.js
+++ server/controllers/scanController.js
@@ -9,7 +9,7 @@ exports.uploadFile = async (req, res) => {
       if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded' });
       }
-  
+   
       const filePath = req.file.path;
 
       const base64Image = convertBase64(filePath);
